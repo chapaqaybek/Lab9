5a7ac55d6f389fe1deccb912f2c74d5af2699ce2
Sardorbek Nu�Monov <s.numonov@172-10-61-182.lightspeed.moblal.sbcglobal.net>
2025-03-13 10:05:57 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+// // Problem 1
+// int multiply(int a, int b) {
+//     if (b == 0) return 0;
+//     if (b < 0) return -multiply(a, -b);
+//     return a + multiply(a, b - 1);
+// }
+
+// // Problem 2
+// int factorial(int n) {
+//     if (n == 0) return 1;
+//     return n * factorial(n - 1);
+// }
+
+// // Problem 3
+// int fibonacci(int n) {
+//     if (n == 0) return 0;
+//     if (n == 1) return 1;
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+
+// // Problem 4
+// int sum(int n) {
+//     if (n == 0) return 0;
+//     return n + sum(n - 1);
+// }
+
+// // Problem 5
+// void printDescending(int n) {
+//     if (n == 0) return;
+//     cout << n << " ";
+//     printDescending(n - 1);
+// }
+
+// Problem 4
+
+// Problem 4
+
+// Problem 4
+
+// Problem 4
+
+// Problem 4
+
+int main() {
+    // // Problem 1
+    // int num1, num2;
+    // cout << "Enter two numbers: ";
+    // cin >> num1 >> num2;
+    //
+    // cout << "Product: " << multiply(num1, num2) << endl;
+    // return 0;
+
+    // // Problem 2
+    // int n;
+    // cout << "Enter a number to calculate factorial: ";
+    // cin >> n;
+    // cout << "Factorial: " << factorial(n) << endl;
+    // return 0;
+
+    // // Problem 3
+    // cout << "Enter a number to find Fibonacci: ";
+    // cin >> n;
+    //
+    // cout << "Fibonacci: " << fibonacci(n) << endl;
+    //
+    // return 0;
+
+    // // Problem 4
+    // int n;
+    // cout << "Enter a number: " << endl;
+    // cin >> n;
+    //
+    // cout <<" Sum of the natural is "<< sum(n) << endl;
+
+    // // Problem 5
+    // int n;
+    // cout << "Enter a number to print in descending order: ";
+    // cin >> n;
+    //
+    // cout << "Descending order: ";
+    // printDescending(n);
+    // cout << endl;
+    // return 0;
+
+    // Problem 6
+
+    // Problem 7
+
+    // Problem 8
+
+    // Problem 9
+
+    // Problem 10
+}
